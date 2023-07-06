5a9faa9c080d32b7850e613931a55fc0e8223304
Neto Nikolay <nikolay.kastranets.1985@gmail.com>
2023-07-06 21:50:54 +0300

    bonus 1 one

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main (String[] args) {
+
+        int ticket = 13676;
+        int mile = 20;
+
+        int bonus = (ticket / mile);
+        System.out.println("Бонусных миль " + bonus);
+    }
+}
